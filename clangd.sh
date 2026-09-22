d0dc447e6616cc38e3b8bf90d5e9581a3a3db59c
silven-mohan <silven667@gmail.com>
2026-09-22 13:46:23 +0530

    Added: Clangd installtion script.

---
+++ clangd.sh
@@ -0,0 +1,10 @@
+~!/bin/bash
+
+## This installs the clangd.
+
+### Download clangd.zip from github releases:
+wget https://github.com/clangd/clangd/releases/download/22.1.6/clangd-linux-22.1.6.zip
+
+
+### Unzip the zip file:
+unzip clangd-linux-22.1.6.zip
